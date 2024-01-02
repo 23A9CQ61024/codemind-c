c469cb0f0f04d1ba6a17af3573d3c2a557ec95aa
23A9CQ61024 <147476350+23A9CQ61024@users.noreply.github.com>
2024-01-02 21:39:37 +0530

    #include <stdio.h> int main() {     int i,c=0;     char str[101];     scanf("%[^ ]",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='A'&&str[i]<='Z') c++;     }     printf("%d",c); }

---
+++ Uppercase_Letters_in_a_String.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i,c=0;
+    char str[101];
+    scanf("%[^
+]",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='A'&&str[i]<='Z') c++;
+    }
+    printf("%d",c);
+}
